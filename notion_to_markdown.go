e59353f24ff83e8ffeabed3c1a417ae6d06ef438
Levent Bekdemir <lvntbkdmr@gmail.com>
2020-02-28 07:36:34 +0000

    removed unnecessary functions

--- notion_to_markdown.go
+++ notion_to_markdown.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"html"
-	"strconv"
-	"strings"
 	"path/filepath"
 
 	"github.com/kjk/notionapi"
@@ -57,76 +55,6 @@ func (c *Converter) getURLAndTitleForBlock(block *notionapi.Block) (string, stri
 	return article.URL(), article.Title
 }
 
-func genGalleryMainHTML(galleryID int, imageURL string) string {
-	s := `
-  <div class="img-wrapper-wrapper">
-    <div class="img-wrapper">
-      <img id="id-gallery-{galleryID}" src="{imageURL}" />
-      <a class="for-nav-icon nav-icon-left" href="#" onclick="imgPrev("{galleryID}"); return false;">
-        <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="nav-icon">
-          <g>
-            <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" class="style-scope yt-icon">
-            </path>
-          </g>
-        </svg>
-      </a>
-      <a class="for-nav-icon nav-icon-right" href="#" onclick="imgNext({galleryID}); return false;">
-        <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="nav-icon" style="">
-          <g>
-            <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" class="yt-icon"></path>
-          </g>
-        </svg>
-      </a>
-    </div>
-  </div>
-`
-	s = strings.Replace(s, "{galleryID}", strconv.Itoa(galleryID), -1)
-	s = strings.Replace(s, "{imageURL}", imageURL, -1)
-	return s
-}
-
-func genGalleryThumbHTML(galleryID int, n int, im *ImageMapping) string {
-	s := `
-    <div id="id-thumb-{galleryID}-{imageNo}" class="pa1 ib">
-      <a href="#" onclick="changeShot({galleryID}, {imageNo}); return false;">
-        <img id="id-thumb-img-{galleryID}-{imageNo}" src="{imageURL}" width="80" height="60" />
-      </a>
-	</div>
-`
-	s = strings.Replace(s, "{galleryID}", strconv.Itoa(galleryID), -1)
-	ns := strconv.Itoa(n)
-	s = strings.Replace(s, "{imageNo}", ns, -1)
-	s = strings.Replace(s, "{imageURL}", im.relativeURL, -1)
-	return s
-}
-
-func (c *Converter) renderGallery(block *notionapi.Block) bool {
-	imageURLS := c.article.getGalleryImages(block)
-	if len(imageURLS) == 0 {
-		return false
-	}
-	panicIf(len(imageURLS) < 2, "expected gallery to have at least 2 images, got %d", len(imageURLS))
-	galleryID := len(c.galleries)
-	c.galleries = append(c.galleries, imageURLS)
-	var images []*ImageMapping
-	for _, link := range imageURLS {
-		im := findImageMapping(c.article.Images, link)
-		panicIf(im == nil, "didn't find ImageMapping for %s", link)
-		images = append(images, im)
-	}
-	firstImage := images[0]
-	s := genGalleryMainHTML(galleryID, firstImage.relativeURL)
-	c.r.Printf(s)
-
-	c.r.Printf(`<div class="center mt3 mb6">`)
-	for i, im := range images {
-		s := genGalleryThumbHTML(galleryID, i, im)
-		c.r.Printf(s)
-	}
-	c.r.Printf(`</div>`)
-	return true
-}
-
 // RenderImage renders BlockImage
 func (c *Converter) RenderImage(block *notionapi.Block) bool {
 	link := block.Source
@@ -156,31 +84,14 @@ func (c *Converter) RenderPage(block *notionapi.Block) bool {
 	return true
 }
 
-// RenderCode renders BlockCode
-func (c *Converter) RenderCode(block *notionapi.Block) bool {
-	// code := html.EscapeString(block.Code)
-	// fmt.Fprintf(g.f, `<div class="%s">Lang for code: %s</div>
-	// <pre class="%s">
-	// %s
-	// </pre>`, levelCls, block.CodeLanguage, levelCls, code)
-	//err := htmlHighlight(c.r.Buf, string(block.Code), block.CodeLanguage, "")
-	//must(err)
-	return true
-}
-
 // if returns false, the block will be rendered with default
 func (c *Converter) blockRenderOverride(block *notionapi.Block) bool {
 	if c.article.shouldSkipBlock(block) {
 		return true
 	}
-	if c.renderGallery(block) {
-		return true
-	}
 	switch block.Type {
 	case notionapi.BlockPage:
 		return c.RenderPage(block)
-	case notionapi.BlockCode:
-		return c.RenderCode(block)
 	case notionapi.BlockImage:
 		return c.RenderImage(block)
 	}
